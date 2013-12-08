06601a9e0f14b54364bb32398144ac32e1608d2a
hoaquynhtim99 <phantandung92@gmail.com>
2013-12-09 00:06:53 +0700

    Module Video Clips 3.4.04

--- modules/video-clip/version.php
+++ modules/video-clip/version.php
@@ -9,22 +9,24 @@
 
 if ( ! defined( 'NV_MAINFILE' ) ) die( 'Stop!!!' );
 
-$module_version = array( //
-	"name" => "Videoclips", //
-	"modfuncs" => "main", //
-	"submenu" => "main", //
-	"is_sysmod" => 0, //
-	"virtual" => 1, //
-	"version" => "3.4.03", //
-	"date" => "Thu, 20 Sep 2012 04:05:46 GMT", //
-	"author" => "PHAN TAN DUNG (phantandung92@gmail.com)", //
-	"uploads_dir" => array( $module_name ), //
-	"note" => "Module playback of video-clips", //
+$module_version = array(
+	"name" => "Videoclips",
+	"modfuncs" => "main",
+	"submenu" => "main",
+	"is_sysmod" => 0,
+	"virtual" => 1,
+	"version" => "3.4.04",
+	"date" => "Thu, 20 Sep 2012 04:05:46 GMT",
+	"author" => "PHAN TAN DUNG (phantandung92@gmail.com)",
+	"uploads_dir" => array( $module_name ),
+	"note" => "Module playback of video-clips",
 	"uploads_dir" => array(
 		$module_name,
 		$module_name . "/icons",
 		$module_name . "/images",
-		$module_name . "/video" ),
-	"files_dir" => array( $module_name ) );
+		$module_name . "/video"
+	),
+	"files_dir" => array( $module_name ) 
+);
 
 ?>
